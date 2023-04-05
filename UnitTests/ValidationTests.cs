6b4d91ec15a1d12bee4cae741da57c7fb25ce00a
jasikor <51225921+jasikor@users.noreply.github.com>
2023-04-05 10:03:42 +0200

    DiceTests separeted

--- UnitTests/ValidationTests.cs
+++ UnitTests/ValidationTests.cs
@@ -2,38 +2,12 @@ using DiceToBip39;
 using LanguageExt;
 using LanguageExt.Common;
 using LanguageExt.UnitTesting;
-using static DiceToBip39.ProgramExt;
 
 
 namespace UnitTests
 {
     public class ValidationTests
     {
-        
-        
-        [Theory]
-        //           0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
-        [InlineData("1234561234561234561234561234561234561234561234561234561234561234561234561234561234561234561234561234")]
-        [InlineData("123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456123412")]
-        public void Create_CorrectlyCreates(string input)
-        {
-            var sut = DiceString.Create(input);
-            sut.ShouldBeSuccess();
-        }
-
-        [Theory]
-        //           0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
-        [InlineData("123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456")]
-        [InlineData("0123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456123456123")]
-        public void Create_Fails(string input)
-        {
-            var sut = DiceString.Create(input);
-            sut.ShouldBeFail();
-        }
-
-        
-        
-
         [Theory]
         [InlineData(new object[] {new string[] {"123456"}})]
         public void SeedParameterSupplied_Validates_SuppliedParameter(string[] input)
