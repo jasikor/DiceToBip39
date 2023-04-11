0a4b0f828e7aa7051db3a545dc23b1d624a6592b
jasikor <51225921+jasikor@users.noreply.github.com>
2023-04-11 13:35:08 +0200

    new tests DiceStringTests.cs

--- UnitTests/DiceStringTests.cs
+++ UnitTests/DiceStringTests.cs
@@ -14,8 +14,18 @@ public class DiceStringTests
     [InlineData("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111123456", 1865)]
     [InlineData("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113", 2)]
     [InlineData("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113666666", 2)]
-    public void DiceToBigInteger_Works(string dice, BigInteger expected) =>
+    public void DiceToBigInteger_Works_OnCorrectData(string dice, BigInteger expected) =>
         DiceString.Create(dice)
             .Map(d => d.ToBigInteger())
             .ShouldBeSuccess(bi => bi.Should().Be(expected));
+    
+    
+    [Theory]
+    [InlineData("1111111")]
+    [InlineData("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")]
+    [InlineData("")]
+    public void DiceToBigInteger_RejectsWrongData(string dice) =>
+        DiceString.Create(dice)
+            .Map(d => d.ToBigInteger())
+            .ShouldBeFail();
 }
